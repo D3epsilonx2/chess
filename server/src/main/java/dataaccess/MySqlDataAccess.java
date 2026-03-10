6716f1567d2c651447be7c4e488dc1a230503af3
D3epsilonx2 <153124501+D3epsilonx2@users.noreply.github.com>
2026-03-10 14:47:15 -0600

    Created MySqlDataAccess. untested for logic and unencrypted password feed.

---
+++ server/src/main/java/dataaccess/MySqlDataAccess.java
@@ -0,0 +1,279 @@
+package dataaccess;
+
+import chess.ChessGame;
+import model.*;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.Collection;
+import com.google.gson.*;
+
+import static java.sql.Types.NULL;
+
+public class MySqlDataAccess implements DAO{
+
+    public MySqlDataAccess() throws DataAccessException  {
+        configureDatabase();
+    }
+
+    //    USERS
+    public void createUser(UserData user) throws DataAccessException {
+        try (Connection conn = DatabaseManager.getConnection()) {
+            var statement = "INSERT INTO users (username, password, email) VALUES (?,?,?)";
+            try (PreparedStatement ps = conn.prepareStatement(statement)){
+                ps.setString(1, user.username());
+                ps.setString(2, user.password());
+                ps.setString(3, user.email());
+                ps.executeUpdate();
+            }
+        } catch (Exception e){
+            throw new DataAccessException(String.format("Unable to read data: %s", e.getMessage()));
+        }
+    }
+
+    public UserData getUser(String username) throws DataAccessException {
+        try (Connection conn = DatabaseManager.getConnection()) {
+            var statement = "SELECT username, password, email FROM users WHERE username=?";
+            try (PreparedStatement ps = conn.prepareStatement(statement)) {
+                ps.setString(1, username);
+                try (ResultSet rs = ps.executeQuery()){
+                    if (rs.next()){
+                        return readUser(rs);
+                    }
+                }
+            }
+        } catch (Exception e) {
+            throw new DataAccessException(String.format("Unable to read data: %s", e.getMessage()));
+        }
+        return null;
+    }
+
+    public Collection<UserData> listUsers() throws DataAccessException {
+        Collection<UserData> result = new ArrayList<>();
+        try (Connection conn = DatabaseManager.getConnection()) {
+            var statement = "SELECT username, password, email FROM users";
+            try (PreparedStatement ps = conn.prepareStatement(statement)) {
+                try (ResultSet rs = ps.executeQuery()) {
+                    while (rs.next()) {
+                        result.add(readUser(rs));
+                    }
+                }
+            }
+        } catch (Exception e) {
+            throw new DataAccessException(String.format("Unable to read data: %s", e.getMessage()));
+        }
+        return result;
+    }
+
+    private UserData readUser(ResultSet rs) throws DataAccessException {
+        try {
+            var username = rs.getString("username");
+            var password = rs.getString("password");
+            var email = rs.getString("email");
+            return new UserData(username, password, email);
+        } catch (Exception e){
+            throw new DataAccessException(String.format("Unable to read data: %s", e.getMessage()));
+        }
+    }
+
+//    GAMEDAO
+
+    public int createGame(GameData game) throws DataAccessException  {
+        var statement = "INSERT INTO games (whiteUsername, blackUsername, gameName, game) VALUES (?,?,?,?)";
+        return executeUpdate(statement, game.whiteUsername(), game.blackUsername(), game.gameName(), game.game());
+    }
+
+    public GameData getGame(int gameID) throws DataAccessException {
+        try (Connection conn = DatabaseManager.getConnection()){
+            var statement = "SELECT gameID, whiteUsername, blackUsername, gameName, game FROM games WHERE gameID=?";
+            try (PreparedStatement ps = conn.prepareStatement(statement)){
+                ps.setInt(1, gameID);
+                try (ResultSet rs = ps.executeQuery()){
+                    if (rs.next()){
+                        return readGame(rs);
+                    }
+                }
+            }
+        } catch (Exception e){
+            throw new DataAccessException(String.format("Unable to read data: %s", e.getMessage()));
+        }
+        return null;
+    }
+
+    public Collection<GameData> listGames() throws DataAccessException {
+        Collection<GameData> gamesList = new ArrayList<>();
+        try (Connection conn = DatabaseManager.getConnection()){
+            var statement = "SELECT gameID, whiteUsername, blackUsername, gameName, game FROM games";
+            try (PreparedStatement ps = conn.prepareStatement(statement)){
+                try (ResultSet rs = ps.executeQuery()){
+                    while (rs.next()){
+                        gamesList.add(readGame(rs));
+                    }
+                }
+            }
+        } catch (Exception e){
+            throw new DataAccessException(String.format("Unable to read data: %s", e.getMessage()));
+        }
+        return gamesList;
+    }
+
+    public void updateGame(GameData game) throws DataAccessException {
+        try (Connection conn = DatabaseManager.getConnection()){
+            var statement = "UPDATE games SET whiteUsername=?, blackUsername=?, game=? WHERE gameID=?";
+            try (PreparedStatement ps = conn.prepareStatement(statement)){
+                ps.setString(1, game.whiteUsername());
+                ps.setString(2, game.blackUsername());
+                ps.setString(3, new Gson().toJson(game.game()));
+                ps.setInt(4, game.gameID());
+                ps.executeUpdate();
+            }
+        } catch (Exception e){
+            throw new DataAccessException(String.format("Unable to read data: %s", e.getMessage()));
+        }
+    }
+
+    private GameData readGame(ResultSet rs) throws DataAccessException {
+        try {
+            var gameID = rs.getInt("gameID");
+            var gameName = rs.getString("gameName");
+            var whiteUsername = rs.getString("whiteUsername");
+            var blackUsername = rs.getString("blackUsername");
+            ChessGame game = new Gson().fromJson(rs.getString("game"), ChessGame.class);
+            return new GameData(gameID, whiteUsername, blackUsername, gameName, game);
+        } catch (Exception e){
+            throw new DataAccessException(String.format("Unable to read data: %s", e.getMessage()));
+        }
+    }
+
+//    AUTHDAO
+
+    public void createAuth(AuthData auth) throws DataAccessException {
+        try (Connection conn = DatabaseManager.getConnection()) {
+            var statement = "INSERT INTO auths (authToken, username) VALUES (?,?)";
+            try (PreparedStatement ps = conn.prepareStatement(statement)){
+                ps.setString(1, auth.authToken());
+                ps.setString(2, auth.username());
+                ps.executeUpdate();
+            }
+        } catch (Exception e){
+            throw new DataAccessException(String.format("Unable to read data: %s", e.getMessage()));
+        }
+    }
+
+    public AuthData getAuth(String authToken) throws DataAccessException {
+        try (Connection conn = DatabaseManager.getConnection()){
+            var statement = "SELECT authToken, username FROM auths WHERE authToken=?";
+            try (PreparedStatement ps = conn.prepareStatement(statement)){
+                ps.setString(1, authToken);
+                try (ResultSet rs = ps.executeQuery()){
+                    if (rs.next()){
+                        return readAuth(rs);
+                    }
+                }
+            }
+        } catch (Exception e){
+            throw new DataAccessException(String.format("Unable to read data: %s", e.getMessage()));
+        }
+        return null;
+    }
+
+    public void deleteAuth(String authToken) throws DataAccessException {
+        var statement = "DELETE FROM auths WHERE authToken=?";
+        try {
+            executeUpdate(statement, authToken);
+        } catch (Exception e){
+            throw new DataAccessException(String.format("Unable to read data: %s", e.getMessage()));
+        }
+    }
+
+    private AuthData readAuth(ResultSet rs) throws DataAccessException {
+        try {
+            var username = rs.getString("username");
+            var authToken = rs.getString("authToken");
+            return new AuthData(authToken, username);
+        } catch (Exception e){
+            throw new DataAccessException(String.format("Unable to read data: %s", e.getMessage()));
+        }
+    }
+
+    public void clear() throws DataAccessException {
+        try {
+            executeUpdate("TRUNCATE TABLE games");
+            executeUpdate("TRUNCATE TABLE users");
+            executeUpdate("TRUNCATE TABLE auths");
+        } catch (Exception e){
+            throw new DataAccessException(String.format("Unable to read data: %s", e.getMessage()));
+        }
+    }
+
+    private int executeUpdate(String statement, Object... params) throws DataAccessException  {
+        try (Connection conn = DatabaseManager.getConnection()){
+            try (PreparedStatement ps = conn.prepareStatement(statement, Statement.RETURN_GENERATED_KEYS)){
+                for (int i = 0; i < params.length; i++){
+                    Object param = params[i];
+                    if (param instanceof String p)ps.setString(i+1, p);
+                    else if (param instanceof Integer p) ps.setInt(i + 1, p);
+                    else if (param instanceof ChessGame p) ps.setString(i + 1, new Gson().toJson(p));
+                    else if (param == null) ps.setNull(i + 1, NULL);
+                }
+                ps.executeUpdate();
+
+                ResultSet rs = ps.getGeneratedKeys();
+                if (rs.next()){
+                    return rs.getInt(1);
+                }
+
+                return 0;
+            }
+        } catch (Exception e){
+            throw new DataAccessException(String.format("Unable to read data: %s", e.getMessage()));
+        }
+    }
+
+    private final String[] createStatements = {
+            """
+            CREATE TABLE IF NOT EXISTS  users (
+              `username` varchar(256) NOT NULL,
+              `password` varchar(256) NOT NULL,
+              `email` varchar(256) NOT NULL,
+              PRIMARY KEY (`username`),
+              INDEX(password),
+              INDEX(email)
+            ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
+            """,
+            """
+            CREATE TABLE IF NOT EXISTS  games (
+              `gameID` int NOT NULL AUTO_INCREMENT,
+              `whiteUsername` varchar(256),
+              `blackUsername` varchar(256),
+              `gameName` varchar(256) NOT NULL,
+              `game` TEXT NOT NULL,
+              PRIMARY KEY (`gameID`),
+              INDEX(whiteUsername),
+              INDEX(blackUsername),
+              INDEX(gameName)
+            ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
+            """,
+            """
+            CREATE TABLE IF NOT EXISTS  auths (
+              `username` varchar(256) NOT NULL,
+              `authToken` varchar(256) NOT NULL,
+              PRIMARY KEY (`authToken`),
+              INDEX(username)
+            ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
+            """
+    };
+
+    private void configureDatabase() throws DataAccessException  {
+        DatabaseManager.createDatabase();
+        try (Connection conn = DatabaseManager.getConnection()) {
+            for (String statement : createStatements) {
+                try (var preparedStatement = conn.prepareStatement(statement)) {
+                    preparedStatement.executeUpdate();
+                }
+            }
+        } catch (Exception e) {
+            throw new DataAccessException(String.format("Unable to read data: %s", e.getMessage()));
+        }
+    }
+}
