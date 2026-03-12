ee51bd5a47499fabaf66edaff924d966888ee2f2
D3epsilonx2 <153124501+D3epsilonx2@users.noreply.github.com>
2026-03-11 19:30:28 -0600

    changed executeUpdate in MySqlDataAccess and added tests to SQLDataAccessTest

--- server/src/test/java/dataaccess/SQLDataAccessTest.java
+++ server/src/test/java/dataaccess/SQLDataAccessTest.java
@@ -20,6 +20,15 @@ public class SQLDataAccessTest {
     }
 
     @Test
+    void createUser() throws DataAccessException {
+        var user = new UserData("BagelBro", "Hotdog95", "BagelBroman@gmail.com");
+        dataAccess.createUser(new UserData(user.username(),user.password(), user.email()));
+
+        Collection<UserData> users = dataAccess.listUsers();
+        assertEquals(1, users.size());
+    }
+
+    @Test
     void getUser() throws DataAccessException {
         var user = new UserData("BagelBro", "Hotdog95", "BagelBroman@gmail.com");
         dataAccess.createUser(new UserData(user.username(),user.password(), user.email()));
@@ -29,6 +38,13 @@ public class SQLDataAccessTest {
 
         Collection<UserData> users = dataAccess.listUsers();
         assertEquals(1, users.size());
+    }
+
+    @Test
+    void getNoUser() throws DataAccessException {
+        var user = new UserData("BagelBro", "Hotdog95", "BagelBroman@gmail.com");
+
+        assertNull(dataAccess.getUser(user.username()));
     }
 
     @Test
@@ -65,10 +81,22 @@ public class SQLDataAccessTest {
     }
 
     @Test
-    public void getGameTest() throws DataAccessException{
+    public void createEvilGameTest() throws DataAccessException{
         var user = new UserData("BagelBro", "Hotdog95", "BagelBroman@gmail.com");
         dataAccess.createUser(new UserData(user.username(),user.password(), user.email()));
         var newCreateGame = new GameData(0, null,
+                null, null, null);
+
+        assertThrows(DataAccessException.class,
+                () -> dataAccess.createGame(newCreateGame)
+        );
+    }
+
+    @Test
+    public void getGameTest() throws DataAccessException{
+        var user = new UserData("BagelBro", "Hotdog95", "BagelBroman@gmail.com");
+        dataAccess.createUser(new UserData(user.username(),user.password(), user.email()));
+        var newCreateGame = new GameData(1, null,
                 null, "aGameYeah", new ChessGame());
 
         var gameID = assertDoesNotThrow(
@@ -79,7 +107,7 @@ public class SQLDataAccessTest {
                 () -> dataAccess.getGame(gameID)
         );
 
-        assertEquals(newCreateGame, gameGet);
+        assertEquals(newCreateGame.gameName(), gameGet.gameName());
     }
 
     @Test
@@ -109,6 +137,68 @@ public class SQLDataAccessTest {
         assertEquals(1, gamesListResult.size());
         GameData result = gamesListResult.stream().findFirst().orElseThrow();
         assertEquals("pizza", result.whiteUsername());
+    }
+
+    @Test
+    void badUpdateGame() throws DataAccessException {
+        var updated = new GameData(1, null,
+                null, null, new ChessGame());
+        dataAccess.updateGame(updated);
+        Collection<GameData> result = dataAccess.listGames();
+        assertEquals(0, result.size());
+    }
+
+    @Test
+    void createAuth() throws DataAccessException {
+        AuthData auth = new AuthData("33", "ham");
+        assertDoesNotThrow(
+                () -> dataAccess.createAuth(auth)
+        );
+        assertEquals(auth.username(), dataAccess.getAuth("33").username());
+    }
+
+    @Test
+    void createEvilAuth() throws DataAccessException {
+        AuthData auth = new AuthData(null, "ham");
+        assertThrows(DataAccessException.class,
+                () -> dataAccess.createAuth(auth)
+        );
+    }
+
+    @Test
+    void getEvilAuth() throws DataAccessException {
+        AuthData auth = new AuthData("33", "ham");
+        assertDoesNotThrow(
+                () -> dataAccess.createAuth(auth)
+        );
+        assertNull(dataAccess.getAuth("32"));
+    }
+
+    @Test
+    void deleteAuth() throws DataAccessException {
+        AuthData auth = new AuthData("33", "ham");
+        assertDoesNotThrow(
+                () -> dataAccess.createAuth(auth)
+        );
+        assertEquals(auth.username(), dataAccess.getAuth("33").username());
+        assertDoesNotThrow(
+                () -> dataAccess.deleteAuth("33")
+        );
+        assertNull(dataAccess.getAuth("33"));
+    }
+
+    @Test
+    void deleteAuthTwice() throws DataAccessException {
+        AuthData auth = new AuthData("33", "ham");
+        assertDoesNotThrow(
+                () -> dataAccess.createAuth(auth)
+        );
+        assertEquals(auth.username(), dataAccess.getAuth("33").username());
+        assertDoesNotThrow(
+                () -> dataAccess.deleteAuth("33")
+        );
+        assertNull(dataAccess.getAuth("33"));
+        assertNull(dataAccess.getAuth("33"));
     }
 
     @Override
