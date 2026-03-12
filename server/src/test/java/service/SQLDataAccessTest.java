ba6141612f7c50474ce210780763c2e3783af92f
D3epsilonx2 <153124501+D3epsilonx2@users.noreply.github.com>
2026-03-11 18:04:29 -0600

    created tests for MySqlDataAccess

---
+++ server/src/test/java/service/SQLDataAccessTest.java
@@ -0,0 +1,111 @@
+package service;
+
+import chess.ChessGame;
+import dataaccess.DataAccessException;
+import dataaccess.MySqlDataAccess;
+import model.*;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.Collection;
+import java.util.Objects;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class SQLDataAccessTest {
+    private MySqlDataAccess dataAccess;
+
+    @BeforeEach
+    void setup() throws Exception {
+        dataAccess = new MySqlDataAccess();
+        dataAccess.clear();
+    }
+
+    @Test
+    void createAndGetUser() throws DataAccessException {
+        var user = new UserData("BagelBro", "Hotdog95", "BagelBroman@gmail.com");
+        dataAccess.createUser(new UserData(user.username(),user.password(), user.email()));
+
+        var result = dataAccess.getUser(user.username());
+        assertNotNull(result);
+
+        Collection<UserData> users = dataAccess.listUsers();
+        assertEquals(1, users.size());
+    }
+
+    @Test
+    void duplicateUser() throws DataAccessException {
+        var user = new UserData("BagelBro", "Hotdog95", "BagelBroman@gmail.com");
+        dataAccess.createUser(new UserData(user.username(),user.password(), user.email()));
+
+        var result = dataAccess.getUser(user.username());
+        assertNotNull(result);
+
+        assertThrows(DataAccessException.class,
+                () -> dataAccess.createUser(new UserData(user.username(),user.password(), user.email()))
+        );
+
+        Collection<UserData> users = dataAccess.listUsers();
+        assertEquals(1, users.size());
+    }
+
+    @Test
+    public void createAndGetGameTest() throws DataAccessException{
+        var user = new UserData("BagelBro", "Hotdog95", "BagelBroman@gmail.com");
+        dataAccess.createUser(new UserData(user.username(),user.password(), user.email()));
+        var newCreateReq = new GameData(0, null,
+                null, "aGameYeah", new ChessGame());
+
+        assertDoesNotThrow(
+                () -> dataAccess.createGame(newCreateReq)
+        );
+
+        Collection<GameData> gamesListResult = assertDoesNotThrow(
+                () -> dataAccess.listGames()
+        );
+        assertEquals(1, gamesListResult.size());
+    }
+
+    @Test
+    void getGameInvalidID() throws DataAccessException {
+        assertNull(dataAccess.getGame(-1));
+    }
+
+    @Test
+    void updateGameTest() throws DataAccessException {
+        var user = new UserData("BagelBro", "Hotdog95", "BagelBroman@gmail.com");
+        dataAccess.createUser(new UserData(user.username(),user.password(), user.email()));
+        var newCreateReq = new GameData(0, null,
+                null, "aGameYeah", new ChessGame());
+
+        var gameID = assertDoesNotThrow(
+                () -> dataAccess.createGame(newCreateReq)
+        );
+        var updated = new GameData(gameID, "pizza",
+                null, "aGameYeah", new ChessGame());
+        assertDoesNotThrow(
+                () -> dataAccess.updateGame(updated)
+        );
+
+        Collection<GameData> gamesListResult = assertDoesNotThrow(
+                () -> dataAccess.listGames()
+        );
+        assertEquals(1, gamesListResult.size());
+        GameData result = gamesListResult.stream().findFirst().orElseThrow();
+        assertEquals("pizza", result.whiteUsername());
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        SQLDataAccessTest that = (SQLDataAccessTest) o;
+        return Objects.equals(dataAccess, that.dataAccess);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(dataAccess);
+    }
+}
