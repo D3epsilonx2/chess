a0012458a22e70bcdcf53069592bff7e6a6288b0
D3epsilonx2 <153124501+D3epsilonx2@users.noreply.github.com>
2026-04-02 04:47:16 -0600

    ServerFacade and ChessClient updates including game relative implementations

--- client/src/main/java/client/ChessClient.java
+++ client/src/main/java/client/ChessClient.java
@@ -1,19 +1,19 @@
 package client;
 
-import com.google.gson.Gson;
-import jdk.jshell.spi.ExecutionControl;
 import model.*;
 import server.ServerFacade;
 import ui.State;
 import ui.EscapeSequences;
 
 import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
 
 public class ChessClient {
+    private final ServerFacade server;
     private String currUsername = null;
     private String currAuth = null;
-    private final ServerFacade server;
+    private List<GameData> gameList = new java.util.ArrayList<>();
     private State state = State.SIGNEDOUT;
 
     public ChessClient(String serverUrl){
@@ -55,17 +55,24 @@ public class ChessClient {
             String[] tokens = input.toLowerCase().split(" ");
             String cmd = (tokens.length > 0) ? tokens[0] : "help";
             String[] params = Arrays.copyOfRange(tokens, 1, tokens.length);
-            return switch (cmd) {
-                case "Quit" -> "quit";
-                case "Login" -> login(params);
-                case "Register" -> register(params);
-                case "Logout" -> signOut();
-                case "CreateGame" -> createGame(params);
-//                case "ListGames" -> listGames();
-                case "PlayGame" -> playGame(params);
-//                case "ObserveGame" -> observe(params);
-                default -> help();
-            };
+            if (state == State.SIGNEDOUT) {
+                return switch (cmd) {
+                    case "quit" -> "quit";
+                    case "login" -> login(params);
+                    case "register" -> register(params);
+                    default -> help();
+                };
+            } else {
+                return switch (cmd) {
+                    case "quit" -> "quit";
+                    case "logout" -> signOut();
+                    case "creategame" -> createGame(params);
+                    case "listgames" -> listGames();
+                    case "playgame" -> joinGame(params);
+                    case "observegame" -> observeGame(params);
+                    default -> help();
+                };
+            }
 //       do error here
         } catch (Exception ex) {
             return ex.getMessage();
@@ -90,7 +97,7 @@ public class ChessClient {
         if (params.length < 3){
             throw new Exception("Error: expected 3 arguments but got" + params.length);
         }
-        AuthData auth = server.register(params[0], params[1], params[3]);
+        AuthData auth = server.register(params[0], params[1], params[2]);
         state = State.SIGNEDIN;
         currUsername = auth.username();
         currAuth = auth.authToken();
@@ -101,27 +108,74 @@ public class ChessClient {
 
     public String signOut() throws Exception {
         assertSignedIn();
+        server.logout(currAuth);
         state = State.SIGNEDOUT;
-        return String.format("You have successfully signed out, %s.", currUsername);
+        String tempName = currUsername;
+        currUsername = null;
+        currAuth = null;
+        return String.format("You have successfully signed out, %s.", tempName);
     }
 
-    public String createGame(String... params) throws ExecutionControl.NotImplementedException {
-        throw new ExecutionControl.NotImplementedException("not implemented");
+    public String createGame(String... params) throws Exception {
+        assertSignedIn();
+
+        if (params.length != 1){
+            throw new Exception("Error: expected 2 arguments but got" + params.length);
+        }
+
+        server.createGame(currAuth, params[0]);
+        return String.format("Created game %s.", params[0]);
     }
 
-//    public String listGames() throws Exception {
-//        assertSignedIn();
-//        String games = server.listGames();
-//        var result = new StringBuilder();
-//        var gson = new Gson();
-//        for (GameData game : games){
-//            result.append(gson.toJson(game)).append('\n');
-//        }
-//        return result.toString();
-//    }
+    public String listGames() throws Exception {
+        assertSignedIn();
+        GameListResult games = server.listGames(currAuth);
+        gameList = new java.util.ArrayList<>(games.games());
+        var result = new StringBuilder();
+        for (int i = 0; i < gameList.size(); i++){
+            GameData game = gameList.get(i);
+            result.append(String.format("%d. %s | White: %s | Black: %s%n",
+                    i + 1,
+                    game.gameName(),
+                    game.whiteUsername() != null ? game.whiteUsername() : "open",
+                    game.blackUsername() != null ? game.blackUsername() : "open"));
+        }
+        return result.toString();
+    }
 
-    public String playGame(String... params) throws ExecutionControl.NotImplementedException {
-        throw new ExecutionControl.NotImplementedException("not implemented");
+    public String joinGame(String... params) throws Exception {
+        assertSignedIn();
+
+        if (gameList.isEmpty()) {
+            throw new Exception("Please run 'listgames' first.");
+        }
+
+        if (params.length < 2){
+            throw new Exception("Error: expected 2 arguments but got" + params.length);
+        }
+
+        int index = Integer.parseInt(params[0]) - 1;
+        if (index < 0 || index >= gameList.size()){
+            throw new Exception("Invalid game number");
+        }
+
+        int gameID = gameList.get(index).gameID();
+        String color = (params[1] != null) ? params[1].toUpperCase() : null;
+        server.joinGame(currAuth, color, gameID);
+        return String.format("Successfully joined game: %s", params[1]);
+    }
+
+    public String observeGame(String... params) throws Exception {
+        assertSignedIn();
+
+        if (gameList.isEmpty()) {
+            throw new Exception("Please run 'listgames' first.");
+        }
+
+        if (params.length != 1){
+            throw new Exception("Error: one parameter expected");
+        }
+        return joinGame(params[0], null);
     }
 
     public String help() {
@@ -130,8 +184,8 @@ public class ChessClient {
                     - Logout
                     - CreateGame <game Name>
                     - ListGames
-                    - PlayGame <game number> <color>
-                    - ObserveGame
+                    - JoinGame <color> <game number>
+                    - ObserveGame <game number>
                     """;
         }
         return """
--- client/src/main/java/server/ServerFacade.java
+++ client/src/main/java/server/ServerFacade.java
@@ -19,24 +19,53 @@ public class ServerFacade {
 
     public AuthData register(String username, String password, String email) throws Exception {
         var user = new RegisterRequest(username, password, email);
-        var request = buildRequest("POST", "/user", user);
+        var request = buildRequest("POST", "/user", user, null);
         var response = sendRequest(request);
         return handleResponse(response, AuthData.class);
     }
 
     public AuthData login(String username, String password) throws Exception {
         var user = new LoginRequest(username, password);
-        var request = buildRequest("POST", "/session", user);
+        var request = buildRequest("POST", "/session", user, null);
         var response = sendRequest(request);
         return handleResponse(response, AuthData.class);
     }
 
-    private HttpRequest buildRequest(String method, String path, Object body) {
+    public void logout(String authToken) throws Exception {
+        var request = buildRequest("DELETE","/session", null, authToken);
+        var response = sendRequest(request);
+        handleResponse(response, null);
+    }
+
+    public GameListResult listGames(String authToken) throws Exception {
+        var request = buildRequest("GET", "/game", null, authToken);
+        var response = sendRequest(request);
+        return handleResponse(response, GameListResult.class);
+    }
+
+    public CreateGameResult createGame(String authToken, String gameName) throws Exception {
+        var body = new CreateGameRequest(authToken, gameName);
+        var request = buildRequest("POST", "/game", body, authToken);
+        var response = sendRequest(request);
+        return handleResponse(response, CreateGameResult.class);
+    }
+
+    public void joinGame(String authToken, String playerColor, int gameID) throws Exception {
+        var body = new GameJoinRequest(playerColor, gameID, authToken);
+        var request = buildRequest("PUT", "/game", body, authToken);
+        var response = sendRequest(request);
+        handleResponse(response, null);
+    }
+
+    private HttpRequest buildRequest(String method, String path, Object body, String authToken) {
         var request = HttpRequest.newBuilder()
                 .uri(URI.create(serverUrl + path))
                 .method(method, makeRequestBody(body));
         if (body != null) {
             request.setHeader("Content-Type", "application/json");
+        }
+        if (authToken != null){
+            request.setHeader("authorization", authToken);
         }
         return request.build();
     }
